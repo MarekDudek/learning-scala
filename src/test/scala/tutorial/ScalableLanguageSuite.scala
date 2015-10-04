c592f9a2d76f2261d5c53d696066790b03d9d7f2
Marek Dudek <marek.dudek@gmail.com>
2015-10-04 12:50:49 +0200

    Access via parentheses: apply and update methods

--- src/test/scala/tutorial/ScalableLanguageSuite.scala
+++ src/test/scala/tutorial/ScalableLanguageSuite.scala
@@ -27,4 +27,48 @@ class ScalableLanguageSuite extends FlatSpec with Matchers {
     val nameHasUpperCase = "Abcde".exists(_.isUpper)
     nameHasUpperCase shouldBe true
   }
+
+  "Iteration over numbers" should "be easy" in {
+
+    var toThree = 0
+    for (i <- 1 to 3)
+      toThree += i
+    toThree shouldBe 6
+
+    var untilThree = 0
+    for (i <- 1 until 3)
+      untilThree += i
+    untilThree shouldBe 3
+
+    var toWithStep = 0
+    for (i <- 1 to 9 by 2)
+      toWithStep += i
+    toWithStep shouldBe 25
+
+    var untilWithStep = 0
+    for (i <- 1 to 9 by 3)
+      untilWithStep += i
+    untilWithStep shouldBe 12
+  }
+
+  "Ranges" should "be fun" in {
+
+    val range = 1 to 10 by 2
+
+    range should contain allOf (1, 3, 5, 7, 9)
+    range should contain noneOf (2, 4, 6, 8, 10)
+  }
+
+  "Access with parentheses" should "be easy to implement" in {
+
+    object Some {
+      def apply(name: String) =
+        name.length
+      def update(index: Int, value: Boolean) =
+        index + (if (value) 1 else 0)
+    }
+
+    Some("name") shouldBe 4
+    (Some(23) = true) shouldBe 24
+  }
 }
